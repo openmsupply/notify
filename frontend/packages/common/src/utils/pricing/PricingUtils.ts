9653dfe95856b96363814c0ac4c66c399823cc06
Lache Melvin <lache.melvin@gmail.com>
2023-08-04 11:15:17 +1200

    removing unused utils

--- frontend/packages/common/src/utils/pricing/PricingUtils.ts
+++
@@ -1,9 +0,0 @@
-export const PricingUtils = {
-  taxAmount: (subtotal: number, total: number) => {
-    return Math.max(total - subtotal, 0);
-  },
-  effectiveTax: (subtotal: number, total: number) => {
-    const taxAmount = PricingUtils.taxAmount(subtotal, total);
-    return (taxAmount / Math.max(subtotal, 1)) * 100;
-  },
-};
