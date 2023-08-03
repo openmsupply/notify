35a7dae23e1745b65cd10795984e516ceedb63b9
Lache Melvin <lache.melvin@gmail.com>
2023-08-04 10:23:40 +1200

    remove currency cells

--- frontend/packages/common/src/ui/layout/tables/components/Cells/index.ts
+++ frontend/packages/common/src/ui/layout/tables/components/Cells/index.ts
@@ -1,7 +1,5 @@
 export * from './TextInputCell';
 export * from './NumberInputCell';
-export * from './CurrencyInputCell';
 export * from './CheckboxCell';
 export * from './ExpiryDateCell';
-export * from './CurrencyCell';
 export * from './CheckCell';
