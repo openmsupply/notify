1a14df5f26b178031010d8d22c3ba8d6e882cc4a
James Brunskill <james@sussol.net>
2023-08-11 13:58:43 +1200

    WIP

--- backend/datasource/src/lib.rs
+++ backend/datasource/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+
+pub fn pg_sql_query_as_json(database_url: String) -> usize {
+    PgConnection::establish(&database_url).unwrap_or_else(|e| panic!("Error connecting to {} : {}", database_url, e));
+
+    return 1;
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::env;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_simple_select() {
+        // EXAMPLE DATABASE_URL=postgres://postgres:postgres@localhost/postgres
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+        assert_eq!(pg_sql_query_as_json(database_url), 1);
+
     }
 }
