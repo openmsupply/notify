9092fa419bd565ae4dd6cde5f09deab03b5d6b3a
Lache Melvin <lache.melvin@gmail.com>
2023-08-14 12:54:21 +1200

    add pagination to recipients; start search and delete toolbar

--- frontend/packages/system/src/Recipients/Recipients/ListView.tsx
+++ frontend/packages/system/src/Recipients/Recipients/ListView.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
 import { useTranslation } from '@common/intl';
-import { Box } from '@common/ui';
-import { RecipientTable } from './RecipientTable';
+import {
+  DataTable,
+  NothingHere,
+  TableProvider,
+  createTableStore,
+  useColumns,
+} from '@common/ui';
 import { useRecipients } from '../api';
-import { Toolbar } from './Toolbar';
 import { useQueryParamsState } from '@common/hooks';
+import { SearchAndDeleteToolbar } from '../../shared/SearchAndDeleteToolbar';
 
 export const ListView = () => {
   const t = useTranslation('system');
-  const { filter, queryParams } = useQueryParamsState();
+  const { filter, queryParams, updatePaginationQuery } = useQueryParamsState();
 
-  const { data: recipients } = useRecipients(queryParams);
+  const columns = useColumns([
+    { key: 'name', label: 'label.name' },
+    { key: 'notificationType', label: 'label.type' },
+    { key: 'toAddress', label: 'label.address' },
+  ]);
+
+  const { data: recipients, isError, isLoading } = useRecipients(queryParams);
+
+  const pagination = {
+    page: queryParams.page,
+    offset: queryParams.offset,
+    first: queryParams.first,
+  };
 
   return (
-    <>
-      <Toolbar data={recipients?.nodes ?? []} filter={filter} />
-      <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
-        <RecipientTable
-          recipients={recipients?.nodes ?? []}
-          nothingHereMessage={t('error.no-recipients')}
-        />
-      </Box>
-    </>
+    <TableProvider createStore={createTableStore}>
+      <SearchAndDeleteToolbar
+        data={recipients?.nodes ?? []}
+        filter={filter}
+        deleteItem={() => Promise.resolve(undefined)}
+      />
+      <DataTable
+        columns={columns}
+        data={recipients?.nodes ?? []}
+        isError={isError}
+        isLoading={isLoading}
+        noDataElement={<NothingHere body={t('error.no-recipients')} />}
+        pagination={{ ...pagination, total: recipients?.totalCount }}
+        onChangePage={updatePaginationQuery}
+      />
+    </TableProvider>
   );
 };
---
+++ frontend/packages/system/src/shared/SearchAndDeleteToolbar.tsx
@@ -0,0 +1,137 @@
+import React, { useEffect, useRef } from 'react';
+import {
+  useNotification,
+  DropdownMenu,
+  DropdownMenuItem,
+  useTranslation,
+  DeleteIcon,
+  useTableStore,
+  AppBarContentPortal,
+  FilterController,
+  AlertModal,
+  useConfirmationModal,
+  SearchBar,
+  LocalStorage,
+  RecordWithId,
+} from '@notify-frontend/common';
+
+type DeleteError = {
+  // TODO: name?
+  id: string;
+  message: string;
+};
+
+export const SearchAndDeleteToolbar = <T extends RecordWithId>({
+  data,
+  filter,
+  deleteItem,
+  searchFilterKey = 'search',
+}: {
+  data: T[];
+  filter: FilterController;
+  deleteItem: (item: T) => Promise<void>;
+  searchFilterKey?: string;
+}) => {
+  const t = useTranslation(['system']);
+  const { success, info } = useNotification();
+
+  const [deleteErrors, setDeleteErrors] = React.useState<DeleteError[]>([]);
+
+  const { selectedRows } = useTableStore(state => ({
+    selectedRows: Object.keys(state.rowState)
+      .filter(id => state.rowState[id]?.isSelected)
+      .map(selectedId => data?.find(({ id }) => selectedId === id))
+      .filter(Boolean) as T[],
+  }));
+
+  const deleteAction = () => {
+    if (selectedRows.length) {
+      const errors: DeleteError[] = [];
+      Promise.all(
+        selectedRows.map(async item => {
+          await deleteItem(item).catch(err => {
+            errors.push({
+              id: item.id,
+              message: err.message,
+            });
+          });
+        })
+      ).then(() => {
+        setDeleteErrors(errors);
+        // Separate check for authorisation error, as this is handled globally i.e. not caught above.
+        // Not using useLocalStorage here, as hook result only updates on re-render (after this function finishes running!)
+        const authError = LocalStorage.getItem('/auth/error');
+        if (errors.length === 0 && !authError) {
+          const deletedMessage = t('messages.deleted-generic', {
+            count: selectedRows.length,
+          });
+          const successSnack = success(deletedMessage);
+          successSnack();
+        }
+      });
+    } else {
+      const selectRowsSnack = info(t('messages.select-rows-to-delete'));
+      selectRowsSnack();
+    }
+  };
+
+  const showDeleteConfirmation = useConfirmationModal({
+    onConfirm: deleteAction,
+    message: t('messages.confirm-delete-generic', {
+      count: selectedRows.length,
+    }),
+    title: t('heading.are-you-sure'),
+  });
+
+  const ref = useRef(deleteAction);
+
+  useEffect(() => {
+    ref.current = deleteAction;
+  }, [selectedRows]);
+
+  const filterString = (filter.filterBy?.[searchFilterKey] as string) || '';
+
+  return (
+    <AppBarContentPortal
+      sx={{
+        paddingBottom: '16px',
+        flex: 1,
+        justifyContent: 'space-between',
+        display: 'flex',
+      }}
+    >
+      <AlertModal
+        message={
+          <ul>
+            {deleteErrors.map(({ id, message }) => (
+              <li key={id}>
+                {id}: {message}
+              </li>
+            ))}
+          </ul>
+        }
+        title={t('messages.error-deleting-generic', {
+          count: deleteErrors.length,
+        })}
+        open={deleteErrors.length > 0}
+        onOk={() => setDeleteErrors([])}
+      />
+      <SearchBar
+        placeholder={t('placeholder.search')}
+        value={filterString}
+        onChange={newValue => {
+          filter.onChangeStringRule(searchFilterKey, newValue);
+        }}
+      />
+      <DropdownMenu label={t('label.select')}>
+        <DropdownMenuItem
+          disabled={!selectedRows.length}
+          IconComponent={DeleteIcon}
+          onClick={() => showDeleteConfirmation()}
+        >
+          {t('button.delete-lines')}
+        </DropdownMenuItem>
+      </DropdownMenu>
+    </AppBarContentPortal>
+  );
+};
