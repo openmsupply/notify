a6ad89a4df79894f20b7ca0e84df46bb9279c705
Lache Melvin <lache.melvin@gmail.com>
2023-08-22 12:21:20 +1200

    add test (recreating recipient with existing to_address)

--- backend/service/src/recipient/tests/delete.rs
+++ backend/service/src/recipient/tests/delete.rs
@@ -4,8 +4,12 @@ mod recipient_delete_test {
 
     use repository::mock::{mock_recipient_a, mock_recipient_b};
     use repository::{mock::MockDataInserts, test_db::setup_all};
-    use repository::{EqualFilter, RecipientFilter, RecipientRepository};
+    use repository::{
+        EqualFilter, NotificationType, RecipientFilter, RecipientRepository, RecipientRowRepository,
+    };
+    use util::uuid::uuid;
 
+    use crate::recipient::create::CreateRecipient;
     use crate::recipient::delete::DeleteRecipientError;
     use crate::service_provider::ServiceContext;
     use crate::service_provider::ServiceProvider;
@@ -95,5 +99,49 @@ mod recipient_delete_test {
                 .unwrap(),
             vec![]
         );
+    }
+
+    #[actix_rt::test]
+    async fn recipient_service_delete_and_recreate() {
+        let (_, _, connection_manager, _) = setup_all(
+            "recipient_service_delete_and_recreate",
+            MockDataInserts::none().recipient_list_members(),
+        )
+        .await;
+
+        let connection = connection_manager.connection().unwrap();
+        let recipient_row_repository = RecipientRowRepository::new(&connection);
+        let service_provider = Arc::new(ServiceProvider::new(
+            connection_manager,
+            get_test_settings(""),
+        ));
+        let context = ServiceContext::new(service_provider).unwrap();
+        let service = &context.service_provider.recipient_service;
+
+        assert_eq!(
+            service.delete_recipient(&context, &mock_recipient_a().id),
+            Ok(mock_recipient_a().id.clone())
+        );
+
+        let new_id = uuid();
+        let recreate_result = service.create_recipient(
+            &context,
+            CreateRecipient {
+                id: new_id.clone(),
+                name: "recreated recipient A".to_string(),
+                to_address: mock_recipient_a().to_address.clone(),
+                notification_type: NotificationType::Email,
+            },
+        );
+
+        if !recreate_result.is_ok() {
+            println!("Error: {:?}", recreate_result);
+        }
+        assert!(recreate_result.is_ok());
+
+        let new_recipient = recipient_row_repository.find_one_by_id(&new_id).unwrap();
+
+        // Recipient now exists
+        assert!(new_recipient.is_some());
     }
 }
