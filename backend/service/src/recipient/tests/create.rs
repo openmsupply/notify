fd47666654b52962f27687cbf9038538335f7e0e
Lache Melvin <lache.melvin@gmail.com>
2023-08-22 12:56:47 +1200

    add test for creating recipient (same to_address diff type)

--- backend/service/src/recipient/tests/create.rs
+++ backend/service/src/recipient/tests/create.rs
@@ -110,4 +110,46 @@ mod recipient_create_test {
         // Recipient email lowercased correctly
         assert_eq!(result.unwrap().to_address, "new_recipient@test.com");
     }
+
+    #[actix_rt::test]
+    async fn create_recipient_same_to_address_different_type_success() {
+        let (_, _, connection_manager, _) = setup_all(
+            "create_recipient_same_to_address_different_type_success",
+            MockDataInserts::none().recipients(),
+        )
+        .await;
+
+        let connection = connection_manager.connection().unwrap();
+        let recipient_row_repository = RecipientRowRepository::new(&connection);
+        let service_provider = Arc::new(ServiceProvider::new(
+            connection_manager,
+            get_test_settings(""),
+        ));
+        let context = ServiceContext::as_server_admin(service_provider).unwrap();
+        let service = &context.service_provider.recipient_service;
+
+        let new_recipient_id = uuid();
+        let result = service.create_recipient(
+            &context,
+            CreateRecipient {
+                id: new_recipient_id.clone(),
+                name: "new_recipient".to_string(),
+                to_address: mock_recipient_a().to_address.clone(),
+                // mock_recipient_a is Email type, so same to_address for Telegram type should succeed
+                notification_type: NotificationType::Telegram,
+            },
+        );
+
+        if !result.is_ok() {
+            println!("Error: {:?}", result);
+        }
+        assert!(result.is_ok());
+
+        let result = recipient_row_repository
+            .find_one_by_id(&new_recipient_id)
+            .unwrap();
+
+        // Recipient now exists
+        assert!(result.is_some());
+    }
 }
