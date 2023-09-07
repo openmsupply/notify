1bb16a5e6ab7d3c25939cba98e068509c62bd2c8
James Brunskill <james@sussol.net>
2023-09-08 10:02:11 +1200

    Clarify Parameters

--- backend/repository/migrations/2023-09-01-003806_sql_recipient_list/up.sql
+++ backend/repository/migrations/2023-09-01-003806_sql_recipient_list/up.sql
@@ -4,7 +4,7 @@ CREATE TABLE
         name TEXT NOT NULL,
         description TEXT NOT NULL,
         query TEXT NOT NULL,
-        parameters TEXT NOT NULL, -- JSON e.g. {"region":"string","tags":"string[]", "limit": "number"}
+        parameters TEXT NOT NULL, -- JSON array of strings ["region","user.name", "user.email"] Note: all params are assumed to be strings, you'll need to cast in your query if you need a float or int
         created_at TIMESTAMP NOT NULL,
         updated_at TIMESTAMP NOT NULL
     );
