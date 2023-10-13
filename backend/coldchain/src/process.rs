db47fe40f76a7b0fdc8ea3bfb25a28b213ab89ec
James Brunskill <james@sussol.net>
2023-10-13 15:44:44 +1300

    Consistent date time between High and Low Temp alerts

--- backend/coldchain/src/process.rs
+++ backend/coldchain/src/process.rs
@@ -252,7 +252,7 @@ fn try_process_coldchain_notifications(
                     location_name: sensor_row.location_name.clone(),
                     sensor_id: sensor_id.clone(),
                     sensor_name: sensor_row.sensor_name.clone(),
-                    datetime: Local::now().naive_local(),
+                    datetime: Local::now().naive_local(), // TODO: Should this be last data time?
                     temperature: current_temp,
                     alert_type: "High".to_string(),
                 }),
@@ -268,7 +268,7 @@ fn try_process_coldchain_notifications(
                     location_name: sensor_row.location_name.clone(),
                     sensor_id: sensor_id.clone(),
                     sensor_name: sensor_row.sensor_name.clone(),
-                    datetime: now,
+                    datetime: Local::now().naive_local(), // TODO: Should this be last data time?
                     temperature: current_temp,
                     alert_type: "Low".to_string(),
                 }),
