77cc5777141da0820e04886333189e4dc58f5e05
James Brunskill <james@sussol.net>
2023-10-13 15:40:10 +1300

    send_temperature_alert -> queue_temperature_alert

--- backend/coldchain/src/process.rs
+++ backend/coldchain/src/process.rs
@@ -8,7 +8,7 @@ use service::{
 };
 
 use crate::{
-    alerts::{send_temperature_alert, TemperatureAlert},
+    alerts::{queue_temperature_alert, TemperatureAlert},
     latest_temperature::{self, latest_temperature},
     parse::ColdChainPluginConfig,
     sensor_info::sensor_info,
@@ -309,7 +309,7 @@ fn try_process_coldchain_notifications(
 
     for alert in alerts {
         // Send the notifications
-        let result = send_temperature_alert(
+        let result = queue_temperature_alert(
             ctx,
             Some(notification_config.id.clone()),
             alert,
