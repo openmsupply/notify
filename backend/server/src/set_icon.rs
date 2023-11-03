de20f64f06e70cbaa11874c2a235cd74f17792be
raviSussol <ravi.shrestha@sussol.net>
2023-11-03 12:10:27 +0545

    47 Rename icon file name and its associated places

--- backend/server/src/set_icon.rs
+++ backend/server/src/set_icon.rs
@@ -4,7 +4,7 @@ extern crate winres;
 #[cfg(windows)]
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("omSupply.ico");
+    res.set_icon("notify.ico");
     // The winres crate compiles the icon into a Windows resource file and directs cargo to link it into the output binary
     res.compile().unwrap();
 }
