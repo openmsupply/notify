3cdbd366d3151bb37dc8e809413a74fafa3ffa0b
James Brunskill <james@sussol.net>
2023-11-09 16:26:58 +1300

    Print error level from build

--- build/windows/notify-build.bat
+++ build/windows/notify-build.bat
@@ -16,12 +16,13 @@ IF NOT EXIST version.txt (
     node -p "require('./frontend/package.json').version" > version.txt
 )
 
-ECHO ##### Prepare notify frontend #####
+@ECHO ##### Prepare notify frontend #####
 set /p APP_BUILD_VERSION=<version.txt
 echo %APP_BUILD_VERSION%
 cd "frontend" && yarn install --force --frozen-lockfile && yarn build-windows
-if %errorlevel% neq 0 exit /b %errorlevel%
+echo %ERRORLEVEL%
+@REM if %errorlevel% neq 0 exit /b %errorlevel%
 
-ECHO ##### Building notify backend #####
+@ECHO ##### Building notify backend #####
 cd "..\backend" && cargo build --release --bin notify_service && copy "target\release\notify_service.exe" "..\notify\notify_service.exe"
 if %errorlevel% neq 0 exit /b %errorlevel%
