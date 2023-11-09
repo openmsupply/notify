ff380e745edf2b67ee19d10db08bc694c5fdeb53
James Brunskill <james@sussol.net>
2023-11-09 14:35:13 +1300

    Apply suggestions from code review
    Co-authored-by: Mark Prins <9192912+mark-prins@users.noreply.github.com>

--- frontend/packages/system/src/NotificationEvents/DetailView/DetailView.tsx
+++ frontend/packages/system/src/NotificationEvents/DetailView/DetailView.tsx
@@ -48,8 +48,7 @@ export const DetailView = () => {
       <AppBarButtonsPortal>
         {/* if we have a config_id, create a link to edit the config */}
         {entity?.notificationConfigId && (
-          <LoadingButton
-            isLoading={isLoading}
+          <BaseButton
             onClick={() => {
               navigate(
                 configRoute(
@@ -62,68 +61,67 @@ export const DetailView = () => {
             endIcon={<EditIcon />}
           >
             {t('button.edit-config')} ({entity.notificationConfig?.title})
-          </LoadingButton>
+          </BaseButton>
         )}
       </AppBarButtonsPortal>
       <AppBarContentPortal sx={{ paddingBottom: '16px', flex: 1 }}>
-        <Box display="flex" flexDirection="row" gap={1} width="100%">
-          <Box gap={1} alignItems="flex" display="flex" flex={1}>
-            <Box flex={1} display="flex">
-              <Box flex={0.2} gap={1}>
-                <Stack gap={1}>
-                  <NotificationStatusChip
-                    status={entity?.status ?? EventStatus.Errored}
-                  />
-                  <RelativeTimeDate d={entity?.sentAt} />
-                </Stack>
-              </Box>
-              <Box flex={1} justifyContent="center" display="flex" gap={1}>
-                {entity?.errorMessage ? (
-                  <TextArea value={entity?.errorMessage} />
-                ) : (
-                  <Typography variant="body1">No Error</Typography>
-                )}
-              </Box>
-              <Box flex={1} justifyContent="right" display="flex" gap={1}>
-                <Stack gap={1}>
-                  <Typography variant="body1">
-                    Created:
-                    <RelativeTimeDate d={entity?.createdAt} />
-                  </Typography>
-                  <Typography variant="body1">
-                    Updated:
-                    <RelativeTimeDate d={entity?.updatedAt} />
-                  </Typography>
-                </Stack>
-              </Box>
-            </Box>
+        <Box flex={1} display="flex" gap={2}>
+          <Box flex={0.2}>
+            <Stack gap={1}>
+              <NotificationStatusChip
+                status={entity?.status ?? EventStatus.Errored}
+              />
+              <RelativeTimeDate d={entity?.sentAt} />
+            </Stack>
+          </Box>
+          <Box flex={1} justifyContent="center" display="flex" gap={1}>
+            {entity?.errorMessage ? (
+              <TextArea value={entity?.errorMessage} />
+            ) : (
+              <Typography variant="body1">No Error</Typography>
+            )}
+          </Box>
+          <Box flex={1} justifyContent="right" display="flex" gap={1}>
+            <Stack gap={1}>
+              <Typography variant="body1">
+                Created:
+                <RelativeTimeDate d={entity?.createdAt} />
+              </Typography>
+              <Typography variant="body1">
+                Updated:
+                <RelativeTimeDate d={entity?.updatedAt} />
+              </Typography>
+            </Stack>
           </Box>
         </Box>
       </AppBarContentPortal>
       {/* Description/Details section */}
-      <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column' }}>
-        <Box sx={{ flex: '1', overflow: 'auto', padding: '4px' }}>
-          {isLoading ? (
-            <BasicSpinner />
-          ) : (
-            <>
-              <Typography variant="h4">{entity?.title}</Typography>
-              <Typography variant="h6">
-                {entity?.toAddress} ({entity?.notificationType})
-              </Typography>
-              <TextArea
-                minRows={2}
-                maxRows={25}
-                sx={{
-                  border: '1px solid',
-                  borderColor: 'grey.100',
-                  width: '100%',
-                }}
-                value={entity?.message}
-              />
-            </>
-          )}
-        </Box>
+<Box
+        sx={{ width: '100%' }}
+        padding={2}
+        display="flex"
+        flexDirection="column"
+      >
+        {isLoading ? (
+          <BasicSpinner />
+        ) : (
+          <>
+            <Typography variant="h4">{entity?.title}</Typography>
+            <Typography variant="h6">
+              {entity?.toAddress} ({entity?.notificationType})
+            </Typography>
+            <TextArea
+              minRows={2}
+              maxRows={25}
+              sx={{
+                border: '1px solid',
+                borderColor: 'grey.100',
+                width: '100%',
+              }}
+              value={entity?.message}
+            />
+          </>
+        )}
       </Box>
     </>
   );
