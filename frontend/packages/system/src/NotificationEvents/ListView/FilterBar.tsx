9c0ce410218e59b5eea754228b32e618972be4ff
Lache Melvin <lache.melvin@gmail.com>
2023-11-21 14:54:33 +1300

    add filter by config button to filter bar

--- frontend/packages/system/src/NotificationEvents/ListView/FilterBar.tsx
+++ frontend/packages/system/src/NotificationEvents/ListView/FilterBar.tsx
@@ -6,7 +6,14 @@ import {
   Box,
   Select,
   EventStatus,
+  useEditModal,
+  LoadingButton,
+  useUrlQueryParams,
+  FilterIcon,
+  useUrlQuery,
 } from '@notify-frontend/common';
+import { NotificationConfigModal } from './NotificationConfigModal';
+import { useNotificationConfigs } from '../../Notifications/api';
 
 function relativeStartTime(hoursAgo: string) {
   return new Date(Date.now() - parseInt(hoursAgo) * 60 * 60 * 1000);
@@ -23,6 +30,7 @@ export const FilterBar = ({
 }) => {
   const t = useTranslation('system');
   const [timeRange, setTimeRange] = React.useState('all');
+  const { isOpen, onClose, onOpen } = useEditModal();
 
   const filterString = (filter.filterBy?.[searchFilterKey] as string) || '';
 
@@ -75,55 +83,100 @@ export const FilterBar = ({
     return options;
   }, []);
 
-  return (
-    <Box
-      sx={{
-        justifyContent: 'space-between',
-        display: 'flex',
-      }}
-    >
-      <SearchBar
-        placeholder={t('placeholder.search')}
-        value={filterString}
-        onChange={newValue =>
-          filter.onChangeStringRule(searchFilterKey, newValue)
-        }
-      />
+  // // maybe 50 or so is fine if searching.... ugh kind of no...
+  // // TODO: what if more than 1000... there's a Pagination::all() on the back end but not sure how to use from frontend...
+  const { queryParams } = useUrlQueryParams({ rowsPerPage: 1000 });
 
-      <Select
-        value={statusValue()}
-        options={statusOptions}
-        onChange={e => {
-          if (e.target.value === 'all') {
-            filter.onClearFilterRule('status');
-          } else {
-            filter.onChangeStringFilterRule(
-              'status',
-              'equalTo',
-              e.target.value
-            );
-          }
+  // TODO: isError, isLoading
+  // should this go here or the modal?? ... just gotta pass the name and id out...
+  // but if we pass the name out, we need a way of getting the name if its already in the url so...
+  const { data } = useNotificationConfigs(queryParams);
+  const notificationConfigs = data?.nodes ?? [];
+
+  const { urlQuery, updateQuery } = useUrlQuery();
+
+  const setFilterConfig = (id: string) => {
+    updateQuery({ ...urlQuery, notificationConfig: id });
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <NotificationConfigModal
+          isOpen={isOpen}
+          onClose={onClose}
+          setSelectedConfig={setFilterConfig}
+          notificationConfigs={notificationConfigs ?? []}
+        />
+      )}
+      <Box
+        sx={{
+          gap: '14px',
+          justifyContent: 'space-between',
+          display: 'flex',
+          flexWrap: 'wrap',
         }}
-      />
-      <Select
-        value={timeRange}
-        options={timeRangeOptions}
-        onChange={e => {
-          setTimeRange(e.target.value);
-          if (e.target.value === 'all') {
-            filter.onClearFilterRule('createdAt');
-          } else {
-            filter.onChangeDateFilterRule(
-              'createdAt',
-              'afterOrEqualTo',
-              relativeStartTime(e.target.value)
-            );
+      >
+        <SearchBar
+          placeholder={t('placeholder.search')}
+          value={filterString}
+          onChange={newValue =>
+            filter.onChangeStringRule(searchFilterKey, newValue)
           }
-        }}
-      />
-      <Box sx={{ gap: '10px', display: 'flex' }}>
-        <ActionButtons />
+        />
+        <Box
+          sx={{
+            gap: '14px',
+            display: 'flex',
+            alignItems: 'center',
+          }}
+        >
+          <Select
+            value={statusValue()}
+            options={statusOptions}
+            onChange={e => {
+              if (e.target.value === 'all') {
+                filter.onClearFilterRule('status');
+              } else {
+                filter.onChangeStringFilterRule(
+                  'status',
+                  'equalTo',
+                  e.target.value
+                );
+              }
+            }}
+          />
+          <Select
+            value={timeRange}
+            options={timeRangeOptions}
+            onChange={e => {
+              setTimeRange(e.target.value);
+              if (e.target.value === 'all') {
+                filter.onClearFilterRule('createdAt');
+              } else {
+                filter.onChangeDateFilterRule(
+                  'createdAt',
+                  'afterOrEqualTo',
+                  relativeStartTime(e.target.value)
+                );
+              }
+            }}
+          />
+
+          <LoadingButton
+            isLoading={false}
+            startIcon={<FilterIcon />}
+            onClick={() => onOpen()}
+            variant="outlined"
+          >
+            {t('label.filter-by-notification-config')}
+          </LoadingButton>
+
+          <Box sx={{ gap: '10px', display: 'flex' }}>
+            <ActionButtons />
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
