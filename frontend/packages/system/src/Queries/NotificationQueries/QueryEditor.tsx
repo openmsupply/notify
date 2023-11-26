ed826a0398842262dfd16c6012bdec93c8524823
Mai <mai@sussol.net>
2023-11-27 10:37:08 +1300

    Removed an unnecessary line

--- frontend/packages/system/src/Queries/NotificationQueries/QueryEditor.tsx
+++ frontend/packages/system/src/Queries/NotificationQueries/QueryEditor.tsx
@@ -123,7 +123,6 @@ export const QueryEditor = ({
 
   const allParamsSet = TeraUtils.extractParams(draft.query).every(param => {
     return queryParams[param] !== (undefined && "");
-    //if (param) {}
   });
 
   let testQueryButton = (
