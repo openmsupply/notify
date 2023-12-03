aec653373c197a6353669d0ab69012f3ed2c2f21
raviSussol <ravi.shrestha@sussol.net>
2023-12-03 19:05:26 +0545

    162 Update windows build command

--- build/windows/notify-build.bat
+++ build/windows/notify-build.bat
@@ -1,21 +1,16 @@
-@ECHO ##### Removing previous builds #####
-@rmdir "notify" /s /q
-
 @ECHO ##### Starting notify builds #####
 mkdir "notify"
-xcopy "backend\configuration" "notify\configuration" /e /h /c /i
-xcopy "backend\templates" "notify\templates" /e /h /c /i
+xcopy "notify\configuration" "notify\configuration" /e /h /c /i
 
 copy "backend\server\notify.ico" "build\notify.ico"
 xcopy "build\*.*" "notify" /c
 xcopy "build\windows\*.*" "notify" /c
 copy "version.txt" "notify\version.txt"
 
-@ECHO ##### Building notify frontend #####
-start /b /wait build\windows\prepare-frontend.bat
+@ECHO ##### Prepare notify frontend #####
+start /wait /b build\windows\notify-prepare.bat
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
-waitfor /t 60 frontend-build
-@ECHO ##### Building notify backend #####
+ECHO ##### Building notify app #####
 cd backend && cargo build --release --bin notify_service && copy "target\release\notify_service.exe" "..\notify\notify_service.exe"
 if %errorlevel% neq 0 exit /b %errorlevel%
