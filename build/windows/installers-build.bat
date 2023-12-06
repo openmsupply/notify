84fa342677047ae1b9a2db61afd97dc99cd62e22
raviSussol <ravi.shrestha@sussol.net>
2023-12-06 17:26:45 +0545

    162 Update suf location

--- build/windows/installers-build.bat
+++ build/windows/installers-build.bat
@@ -2,9 +2,8 @@
 @rmdir "installers" /s /q
 
 @ECHO ##### Adjusting SUFS #####
-SET installerWorkspace=C:\Program Files (x86)\Jenkins\jobs\notifyMain - Installers\workspace\notify
-SET SUFlocation=%installerWorkspace%\notify
-FOR /F "delims=*" %%i in ('more notify\version.txt') do SET versionTag=%%i
+SET SUFlocation=C:\Program Files (x86)\Jenkins\jobs\notifyMain - Installers\workspace\notify\notify
+FOR /F "delims=*" %%i in ('more notify\notify\version.txt') do SET versionTag=%%i
 @ECHO "current tag = %versionTag%"
 SET installersOutputFolder=%WORKSPACE%\installers
 
@@ -13,4 +12,4 @@ node "%SUFlocation%\adjustSUFs.js"
 @cd ..
 
 @ECHO ##### Creating installers #####
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\notify_service.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%SUFlocation%\notify_service.suf"
