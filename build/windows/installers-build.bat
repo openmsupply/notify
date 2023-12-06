25a0540f5e9ea2a121d2257f426b336f17f3a9c5
raviSussol <ravi.shrestha@sussol.net>
2023-12-06 14:19:10 +0545

    162 Fix setup factory template file path error

--- build/windows/installers-build.bat
+++ build/windows/installers-build.bat
@@ -2,13 +2,16 @@
 @rmdir "installers" /s /q
 
 @ECHO ##### Adjusting SUFS #####
+SET installerWorkspace=C:\Program Files (x86)\Jenkins\jobs\notifyMain - Installers\workspace\notify
+SET SUFlocation=%installerWorkspace%\notify
 FOR /F "delims=*" %%i in ('more notify\version.txt') do SET versionTag=%%i
 @ECHO "current tag = %versionTag%"
 SET installersOutputFolder=%WORKSPACE%\installers
 
 @cd notify
-node "adjustSUFs.js"
+node "%SUFlocation%\adjustSUFs.js"
 @cd ..
 
 @ECHO ##### Creating installers #####
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "notify\notify\notify_service.suf"
+move "notify\notify\build\Final Application\mSupply" "mSupply\mSupply Single user"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\notify_service.suf"
