7abb40b993fada8654e4d52874d4954886042790
raviSussol <ravi.shrestha@sussol.net>
2023-12-06 20:23:35 +0545

    162 Fix install build bat command

--- build/windows/installers-build.bat
+++ build/windows/installers-build.bat
@@ -1,15 +1,19 @@
 @ECHO ##### Removing installers folder #####
 @rmdir "installers" /s /q
 
+@ECHO ##### Update SUF files #####
+copy /y notify\notify\notify_service.suf notify\notify_service.suf
+
 @ECHO ##### Adjusting SUFS #####
-SET SUFlocation=C:\Program Files (x86)\Jenkins\jobs\notifyMain - Installers\workspace\notify\notify
+SET installerWorkspace=%cd%\notify
+SET SUFlocation=%installerWorkspace%\notify
 FOR /F "delims=*" %%i in ('more notify\notify\version.txt') do SET versionTag=%%i
 @ECHO "current tag = %versionTag%"
-SET installersOutputFolder=%WORKSPACE%\installers
+SET installersOutputFolder=%cd%\installers
 
 @cd notify
 node "%SUFlocation%\adjustSUFs.js"
 @cd ..
 
 @ECHO ##### Creating installers #####
-start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%SUFlocation%\notify_service.suf"
+start "" /wait "C:\Program Files (x86)\Setup Factory 9\SUFDesign.exe" /BUILD /LOG:installers\setup-factory.log "%installerWorkspace%\notify_service.suf"
