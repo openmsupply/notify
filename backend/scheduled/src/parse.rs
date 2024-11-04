6b6e6dba19c32cfe9486ee5e06afb9d06b76d51f
Ethan McQuarrie <ethan.m@msupply.foundation>
2024-11-04 14:31:32 +1300

    Fixup lookforward issue

--- backend/scheduled/src/parse.rs
+++ backend/scheduled/src/parse.rs
@@ -91,15 +91,17 @@ impl ScheduledNotificationPluginConfig {
                 return Ok(next_due_date);
             }
             "monthly" => {
+                let mut look_forward = 1;
                 let mut next_due_date = self.schedule_start_time;
                 while next_due_date < now_utc {
                     // Note: chrono automatically handles leap years and returns last day of the month if day isn't valid for that month
                     // https://docs.rs/chrono/latest/chrono/naive/struct.NaiveDateTime.html#method.checked_add_months
-                    let option = next_due_date.checked_add_months(Months::new(1));
+                    let option = self.schedule_start_time.checked_add_months(Months::new(look_forward));
                     next_due_date = match option {
                         Some(d) => d,
                         None => return Err(NotificationError::InvalidNextDueDate),
                     };
+                    look_forward += 1;
                 }
                 return Ok(next_due_date);
             }
